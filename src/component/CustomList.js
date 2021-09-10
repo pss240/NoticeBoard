ac572b90d203c86a261daa4a4306d0280057a166
pss <arp11150@gmail.com>
2021-09-11 07:18:20 +0900

    branch new customlist

---
+++ src/component/CustomList.js
@@ -0,0 +1,14 @@
+import react,{ Component } from "react";
+
+
+class CustomList extends Component{
+    render(){
+        return(
+                <div>
+
+                </div>
+        );
+    }
+}
+
+export default CustomList;
